2167f87d4f9ed492fc4f5f90c6e3f1103b03b541
DEO PRAKASH <deo.prakash@ssipmt.com>
2025-10-22 12:38:48 +0530

    Loops

---
+++ decision.rs
@@ -0,0 +1,40 @@
+fn main(){
+    // let a:i32 = 30;
+    // let b:i32 = 45;
+    // let c:i32 = 50;
+
+    // if a > b {
+    //     println!("a is greater");
+    // }
+
+    // else if b > c{
+    //     println!("b is greater");
+
+    // }else { 
+    //     println!("c is greater");
+    // }
+
+    //----------------- Match Statement -----------------------------  \\
+
+    // let state_code = "MH";
+    // let state = match state_code{
+    //     "MH" => {
+    //         println!("Found match for MH");
+    //         "Maharashtra"},
+    //     "KL" => "Kerala",
+    //     "KA" => "Karnataka",
+    //     "GA" => "GOA",
+    //     _ => "Unknown"
+    // };
+    // println!{"State name is {}", state};
+
+    // let string1 = "han";
+    // let res = match string1{
+    //     "hen" => "Peahen",
+    //     "hat" => "Manhatton",
+    //     "han" => "elephant",
+    //     _ => "Unknown"
+    // };
+
+    // println!("word is {}", res);
+}
